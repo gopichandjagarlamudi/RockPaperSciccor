9d9e60be3059f37485cfeacd89c2aa816a607c39
gopichandjagarlamudi <jagarlamudigopichand99@gmail.com>
2023-01-12 16:11:12 -0500

    written the logic for the game

--- logic.js
+++ logic.js
@@ -14,4 +14,44 @@ function getComputerChoice()
         return "scissor";
     }
 }
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection)
+{
+    if(playerSelection==computerSelection)
+    {
+        return "It is a tie";
+    }
+    else if(playerSelection=="rock")
+    {
+        if(computerSelection=="paper")
+        {
+            return "You Lose! paper beats rock";
+        }
+        else{
+            return "You Win! rock beats scissor";
+        }
+    }
+    else if(playerSelection=="paper")
+    {
+        if(computerSelection=="rock")
+        {
+            return "You Win! paper beats rock";
+        }
+        else{
+            return "You lose! scissors beats rock";
+        }
+    }
+    else if(playerSelection=="scissors")
+    {
+        if(computerSelection=="rock")
+        {
+            return "You Lose! rock beats scissors";
+        }
+        else{
+            return "You Win! scissors beats paper";
+        }
+    }
+
+}
+const computerSelection=getComputerChoice();
+const playerSelection="rock"; 
+console.log(playRound(playerSelection, computerSelection));
